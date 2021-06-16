6407179c292800afcf34dd16a2fcd68481c562af
KostaGorod <35299380+KostaGorod@users.noreply.github.com>
2021-06-16 15:51:22 +0300

    Update aliasList.go

--- cmd/aliasList.go
+++ cmd/aliasList.go
@@ -3,7 +3,7 @@ package cmd
 import (
 	"os"
 
-	"github.com/drlogout/iredmail-cli/iredmail"
+	"github.com/kostagorod/iredmail-cli/iredmail"
 	"github.com/olekukonko/tablewriter"
 	"github.com/spf13/cobra"
 )
