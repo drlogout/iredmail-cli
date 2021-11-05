e8ec4dea0b865e9a7baba20c2f9af37be4a2f1c1
noxpost <41703267+noxpost@users.noreply.github.com>
2021-11-05 07:41:56 -0500

    Update aliasList.go

--- cmd/aliasList.go
+++ cmd/aliasList.go
@@ -3,7 +3,7 @@ package cmd
 import (
 	"os"
 
-	"github.com/KostaGorod/iredmail-cli/iredmail"
+	"github.com/noxpost/iredmail-cli/iredmail"
 	"github.com/olekukonko/tablewriter"
 	"github.com/spf13/cobra"
 )
