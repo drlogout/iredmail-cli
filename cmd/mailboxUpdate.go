76b3057f5a64fa7f29c721b32d115f2e8d2cf968
noxpost <41703267+noxpost@users.noreply.github.com>
2021-11-05 07:47:04 -0500

    Update mailboxUpdate.go

--- cmd/mailboxUpdate.go
+++ cmd/mailboxUpdate.go
@@ -20,7 +20,7 @@ import (
 	"strconv"
 
 	"github.com/asaskevich/govalidator"
-	"github.com/KostaGorod/iredmail-cli/iredmail"
+	"github.com/noxpost/iredmail-cli/iredmail"
 	"github.com/spf13/cobra"
 )
 
